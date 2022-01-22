24dcae165923b61ba4d767cb2e5909b81f917aeb
Josh Simmons <josh.simmons@shopify.com>
2022-01-22 06:20:32 -0700

    add auth

---
+++ blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "../../../lib/prisma";
+
+import NextAuth from "next-auth";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+});
